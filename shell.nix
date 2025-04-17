6c4259d05d40c987526b50a4ef6a589e5fb8c795
wrvsrx <wrvsrx@outlook.com>
2025-04-17 13:54:15 +0800

    chore(env): add python and uv

--- shell.nix
+++ shell.nix
@@ -1,1 +1,16 @@
-{ mkShell }: mkShell { buildInputs = [ ]; }
+{
+  mkShell,
+  uv,
+  python3,
+}:
+mkShell {
+  env = {
+    UV_LINK_MODE = "copy";
+    UV_PYTHON = "${python3}/bin/python";
+    UV_PYTHON_DOWNLOADS = "never";
+  };
+  nativeBuildInputs = [
+    uv
+    python3
+  ];
+}
